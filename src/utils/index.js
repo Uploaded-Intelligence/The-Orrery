194a5782b668ef79f7315f5ac261da1c56ba6ebf
Uploaded-Intelligence <148705917+Uploaded-Intelligence@users.noreply.github.com>
2026-01-13 18:47:14 +0800

    feat: export DAG layout utilities

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,4 @@ export { isTaskLocked, getComputedTaskStatus, getAvailableTasks } from './tasks.
 export { getQuestProgress } from './quests.js';
 export { autoLayoutDAG, getLayoutPositions, getEdgePath, getCanvasBounds, LAYOUT } from './layout.js';
 export { taskToMarkdown, stateToObsidianManifest } from './toObsidian.js';
+export { computeLayers, computeLayout } from './forceLayout.js';
